8f6918a5f4f256af38a3992722d16ba397a2093b
Midathana Hemasatyaveni <midathanasatya8@gmail.com>
2025-06-15 21:18:56 +0530

    first commit

---
+++ majority_element.py
@@ -0,0 +1,13 @@
+def majorityele(arr):
+    n = len(arr)
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            if arr[i] ==arr[j]:
+                count += 1
+        if count>n//2:
+           
+            return arr[i]
+    return -1
+arr = [2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,3,4]
+print(majorityele(arr))
