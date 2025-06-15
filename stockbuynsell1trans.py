06edc8c9004e9be39bac1937ad9b8fb20a5a6457
Midathana Hemasatyaveni <midathanasatya8@gmail.com>
2025-06-15 20:54:06 +0530

    first commit

--- stockbuynsell1trans.py
+++ stockbuynsell1trans.py
@@ -1,9 +1,18 @@
-def onetrans(arr):
-    n = len(arr)
-    result = 0
-    for i in range(n-1):
-        for j in range(i+1,n):
-            result= max(result,arr[j]-arr[i])
+# def onetrans(arr):
+#     n = len(arr)
+#     result = 0
+#     for i in range(n-1):
+#         for j in range(i+1,n):
+#             result= max(result,arr[j]-arr[i])
+#     return result
+# arr = [7, 10, 1, 3, 6, 9, 2]
+# print(onetrans(arr))
+def onetrans(prices):
+    n = float('inf')
+    result =0
+    for price in prices:
+        n = min(n,price)
+        result = max(result,price- n)
     return result
-arr = [7, 10, 1, 3, 6, 9, 2]
-print(onetrans(arr))
+prices = [7, 10, 1, 3, 6, 9, 2]
+print(onetrans(prices))
