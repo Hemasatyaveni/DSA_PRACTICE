8b98cc299ae0be47ecc70c081b45f11e8afb7b47
Midathana Hemasatyaveni <midathanasatya8@gmail.com>
2025-06-19 11:21:35 +0530

    first

--- containerwithmorewater.py
+++ containerwithmorewater.py
@@ -1,17 +1,17 @@
-def container(height):
+def maxArea(height):
     n = len(height)
-    l = 0
-    r = n-1
-    max_sum = 0
-    while l<r:
-        w = r - l
-        h = min(height[l],height[r])
-        a = w*h
-        max_sum = max(max_sum,a)
-        if height[l]<height[r]:
-            l+=1
+    left = 0
+    right = n - 1
+    max_area = 0
+    while left < right:
+        width = right - left
+        h = min(height[left], height[right])
+        area = width * h
+        max_area = max(max_area, area)
+        if height[left] < height[right]:
+            left += 1
         else:
-            r-=1
-    return max_sum
-height = [1,8,6,2,5,4,8,3,7]
-print(container(height))
+            right -= 1
+    return max_area
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(maxArea(height))
